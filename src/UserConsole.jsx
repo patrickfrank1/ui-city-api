99f8f46d653bd628196569ed899debe28530fafc
Patrick Frank <patr.frank@gmx.de>
2020-09-19 19:53:41 +0200

    Prepare for upload to github and heroku

--- src/UserConsole.jsx
+++ src/UserConsole.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class UserConsole extends React.Component {
   constructor(props) {
@@ -55,13 +57,60 @@ class UserConsole extends React.Component {
   }
 
   handleLogin = () => {
-    this.props.handleMessage("UserConsole not implemented yet.", "warn");
+    if (this.state.username === '') {
+      this.props.handleMessage("Please specify a username.", "error");
+    }
+    if (this.state.password === '') {
+      this.props.handleMessage("Please specify a password.", "error");
+    }
+    const payload = {
+      'username': this.state.username,
+      'password': this.state.password
+    };
+    axios.post('https://city-search-node-api.herokuapp.com/login',payload)
+    .then(res => {
+      console.log(res);
+    });
+    // fetch('https://city-search-node-api.herokuapp.com/login',{
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(payload)
+    // }).then(res => {
+    //   if (res.status >= '400') {
+    //     res.text().then(resText => {
+    //       this.props.handleMessage(resText, "error");
+    //     })
+    //   } else {
+    //     res.text().then(resText => {
+    //       this.props.handleMessage("You are logged in.", "success");
+    //     })
+    //   }
+    // }).catch(err => {
+    //   this.props.handleMessage("Internal server error.", "error");
+    //   console.log(err);
+    // });
+
     this.setState({username: '', password: ''});
   }
 
   handleDelete = () => {
-    this.props.handleMessage("UserConsole not implemented yet.");
-    this.setState({username: '', password: ''});
+    axios.delete('https://city-search-node-api.herokuapp.com/deregister',{
+      withCredentials: true,
+      crossDomain: true
+    }).then(res =>{
+      if(res.status === '200') {
+        res.text().then(resText => {
+          this.props.handleMessage(resText, "success");
+        });
+      } else {
+        res.text().then(resText => {
+          this.props.handleMessage(resText, "error");
+        });
+      }
+    });
   }
 
   render = () => {
